003a8da3d2bf18a235a7a37c1ad777cb72be3260
Vlad <alzwded@gmail.com>
2014-04-18 17:35:05 +0300

    add NOTE to how to enable foreign keys for sqlite
    but fp's current implementation of TSQLite3Connection doesn't let me

--- contacts/schema.sql
+++ contacts/schema.sql
@@ -1,4 +1,5 @@
 -- contacts.sqlite3
+PRAGMA foreign_keys = TRUE ;
 CREATE TABLE IF NOT EXISTS names (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(40) NULL,
@@ -12,6 +13,15 @@ CREATE TABLE IF NOT EXISTS metadata (
     FOREIGN KEY (id) REFERENCES names(id) ON DELETE CASCADE,
     PRIMARY KEY (id, metadata_type)
     );
+-- NOTE: PRAGMA foreign_keys = TRUE ; solves this issue,
+--       but it needs to be enabled per each connection.
+--       Sadly, this is isn't possible on the fpc version I use to
+--       compile this turkey because a transaction is always open
+--       and that PRAGMA within a transactino is a no-op.
+--       However, it became an option for the TSQLite3Connection
+--       object in fpc 2.7.1. So, one day we will be able to
+--       delete this turkey.
+--       
 -- apparently ON DELETE CASCADE doesn't actually deliver...
 CREATE TRIGGER IF NOT EXISTS purge_metadata BEFORE DELETE ON names
     FOR EACH ROW BEGIN
